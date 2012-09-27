ab4be0a9c4c800a2bb5b4aadfae3e0e7ea82c509
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2012-09-28 00:37:21 +0200

    Better mouse-camera control for Earth example

--- content/scripts/examples/earth.js
+++ content/scripts/examples/earth.js
@@ -1,80 +1,85 @@
 /* 1. Load the scene graph manager module into the grid
  */
 grid.send({
-    action: "module.load",
-    modules: ["scenes/scene"],
-    configs: {
-        canvasId: "theCanvas"
-    }
-},
-        function() {
+        action:"module.load",
+        modules:["scenes/scene"],
+        configs:{
+            canvasId:"theCanvas"
+        }
+    },
+    function () {
 
-            /* 2. Load modules for managing camera, mouse and earth
-             *    which will use resources provided by the scene manager module
-             */
+        /* 2. Load modules for managing camera, mouse and earth
+         *    which will use resources provided by the scene manager module
+         */
 
-            grid.send({
-                action: "module.load",
-                modules: [
+        grid.send({
+                action:"module.load",
+                modules:[
                     "camera/camera",
                     "input/mouse",
                     "objects/planets/earth",
                     "objects/skies/milkyway"
                 ]
             },
-                    function() {
+            function () {
 
-                        grid.send({
-                            action:"camera.set",
-                            eye : { x: 0.0, y: 0.0, z: 7 },
-                            look : { y:0.0 },
-                            up : { y: 1.0 }
-                        });
+                /*------------------------------------------------------------------------
+                 * Crude canvas pan control
+                 *----------------------------------------------------------------------*/
 
-                        /* 3. Wire together various events and actions
-                         * defined by the modules
-                         */
+                var x = 0;
+                var y = 0;
 
-                        var rotateX = 0;
-                        var rotateY = 0;
+                var lastX;
+                var lastY;
 
-                        var lastX;
-                        var lastY;
+                var dragging = false;
 
-                        var dragging = false;
+                /* Set initial camera state
+                 */
+                grid.send({
+                    action:"camera.set",
+                    eye:{ x:x, y:y, z:5 },
+                    look:{ y:0.0 },
+                    up:{ y:1.0 }
+                });
 
-                        grid.onEvent(
-                                "mouse.down",
-                                function(params) {
-                                    lastX = params.canvasX;
-                                    lastY = params.canvasY;
-                                    dragging = true;
-                                });
-
-                        grid.onEvent(
-                                "mouse.move",
-                                function(params) {
-
-                                    if (dragging) { // A ridiculously crude drag-pan control
-
-                                        rotateX += params.canvasY - lastY;
-                                        rotateY += params.canvasX - lastX;
-
-                                        grid.send({
-                                            action:"earth.set",
-                                            rotateX: rotateX * 0.3,
-                                            rotateY: rotateY * 0.3
-                                        });
-                                    }
-
-                                    lastX = params.canvasX;
-                                    lastY = params.canvasY;
-                                });
-
-                        grid.onEvent(
-                                "mouse.up",
-                                function(params) {
-                                    dragging = false;
-                                });
+                /* Mouse handlers
+                 */
+                grid.onEvent(
+                    "mouse.down",
+                    function (params) {
+                        dragging = true;
                     });
-        });
+
+                grid.onEvent(
+                    "mouse.move",
+                    function (params) {
+
+                        if (dragging && lastX != null) {
+
+                            x += params.canvasX - lastX;
+                            y += params.canvasY - lastY;
+
+                            grid.send({
+                                action:"camera.set",
+                                eye:{
+                                    x:x * 0.1,
+                                    y:y * 0.1
+                                }
+                            });
+                        }
+
+                        lastX = params.canvasX;
+                        lastY = params.canvasY;
+                    });
+
+                grid.onEvent(
+                    "mouse.up",
+                    function (params) {
+                        dragging = false;
+                        lastX = null;
+                    });
+            });
+    });
